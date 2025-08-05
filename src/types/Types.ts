596b96f8648aa4d6f86e9e2311a3ebcac723d909
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-08-05 11:14:41 +1000

    types

---
+++ src/types/Types.ts
@@ -0,0 +1,27 @@
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type Movies = Movie[];
+
+export type ExternalIds = {
+  facebook_id: string | null;
+  id: number;
+  imdb_id: string | null;
+  instagram_id: string | null;
+  twitter_id: string | null;
+  wikidata_id: string | null;
+}
