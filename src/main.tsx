3e6d7fe27572741716cc9fe5ff555115b76efc99
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-08-05 11:38:52 +1000

    csss

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './styles/bootstrap.min.css'
 import './styles/jquery-mmenu.css'
 import './styles/index.css'
 import './styles/responsive.css'
+import './styles/themify-icons.css'
 import MovieGrid from './pages/MovieGrid'
 
 createRoot(document.getElementById('root')!).render(
