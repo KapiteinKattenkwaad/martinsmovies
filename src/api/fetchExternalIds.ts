ecefc709aabfaf36efd35cb747f12504912b093d
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-08-05 11:15:10 +1000

    Create fetchExternalIds.ts

---
+++ src/api/fetchExternalIds.ts
@@ -0,0 +1,32 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const apiKey = import.meta.env.VITE_MOVIES_TMDB_API_KEY;
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`
+    }
+};
+
+export default async function fetchExternalIds({
+    movieId,
+}: { movieId: number; }) {
+
+    try {
+        const url = `${BASE_URL}/movie/${movieId}/external_ids`
+
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error("Failed to fetch movies");
+        }
+
+        const data = await response.json();
+        return data;
+    }
+
+    catch (error) {
+        console.error(error);
+        return error;
+    }
+}
