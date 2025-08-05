74625a8270ec36ee215c479a051aca3f110e11f7
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-08-05 11:13:52 +1000

    get genres and watched movies

---
+++ src/utils/watchedMovies.ts
@@ -0,0 +1,18 @@
+const STORAGE_KEY = "watchedMovies";
+
+export function getWatched(): number[] {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+}
+
+export function markWatched(id: number) {
+  const watched = getWatched();
+  if (!watched.includes(id)) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([...watched, id]));
+  } else {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify([...watched.filter(watchedId => watchedId !== id)]));
+  }
+}
+
+export function isWatched(id: number): boolean {
+  return getWatched().includes(id);
+}
